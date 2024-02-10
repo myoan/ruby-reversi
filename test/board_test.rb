cac1e0a14ed4c9aa33c14573c6dbad16b741cef7
myoan <motoki.yoan@gmail.com>
2024-02-10 11:07:02 +0900

    test: add Board.filled?

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,14 +1,39 @@
 require "minitest/autorun"
-# require "minitest/unit"
 require "board"
 
-class TestMeme < Minitest::Test
+class TestBoard < Minitest::Test
   def setup
-    @meme = Meme.new
-    @board = Board.new(4)
   end
 
   def test_that_kitty_can_eat
     assert_equal "OHAI!", "OHAI!"
+  end
+end
+
+describe Board do
+  before do
+    @board = Board.new(4)
+  end
+
+  describe "Board.filled?" do
+    describe "when board is not filled" do
+      it "returns false" do
+        _(@board.filled?).must_equal false
+      end
+    end
+
+    describe "when board is filled" do
+      before do
+        @board.board.each.with_index do |line, y|
+          line.each.with_index do |cell, x|
+            @board.put_stone(x, y, Cell::BLACK)
+          end
+        end
+      end
+
+      it "returns true" do
+        _(@board.filled?).must_equal true
+      end
+    end
   end
 end
